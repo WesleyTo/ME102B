6dd3d349e770d67306f7c5245ff9aaeb11be6696
Rachit Nanda <rnanda1243@gmail.com>
2014-03-05 20:15:45 -0800

    2 hall sensors, but can't figure out how to read them in order

--- Hall_test/Hall_test.ino
+++ Hall_test/Hall_test.ino
@@ -1,6 +1,7 @@
  unsigned int rpm;
  unsigned long timeold;
- unsigned int rpmStorage[] = {0,0};
+ unsigned int rpmStorage1[] = {0,0};
+ unsigned int rpmStorage2[] = {0,0};
  byte currPos = 0;
  byte readRpm = 0;
  unsigned int rpms[3] = {0, 1, 2};
@@ -8,19 +9,22 @@
  byte upperLimit = 70;
  byte lowerLimit = 50;
  byte resetVal = 60;
+ byte flag = 0;
 
  void setup()
  {
    Serial.begin(9600);
-   attachInterrupt(0, hallHigh, RISING);
+   attachInterrupt(0, hallHigh1, RISING);
+   attachInterrupt(1, hallHigh2, RISING);
    digitalWrite(2, HIGH);
+   digitalWrite(3, HIGH);
    rpm = 0;
    timeold = 0;
  }
  
  unsigned int getRpm(){
    if (readRpm == 2) {
-    return 60000/(rpmStorage[(currPos+1)%2] - rpmStorage[currPos]); 
+    return rpmStorage2[currPos] - rpmStorage1[currPos]; 
    }
    else{
      return 0;
@@ -29,9 +33,9 @@
  
  void storeRpm() {
    rpms[storePos] = getRpm();
-   if (rpms[storePos] != rpms[(storePos+2)%3]) {
+   //if (rpms[storePos] != rpms[(storePos+2)%3]) {
      Serial.println(rpms[storePos]); 
-   }
+   //}
    storePos = (storePos + 1) % 3;
  }
 
@@ -40,12 +44,29 @@
    storeRpm();
  }
 
- void hallHigh()
+ void hallHigh1()
  {
-   if (readRpm < 2) {
-     readRpm++;
+   if (!flag){
+      flag = 1;
+      rpmStorage1[currPos] = millis();
    }
-   rpmStorage[currPos] = millis();
-   currPos = (currPos + 1) % 2;
  }
+ 
+  void hallHigh2()
+ {
+
+   if (flag) {
+     if (readRpm < 2) {
+       readRpm++;
+     }
+     rpmStorage2[currPos] = millis();
+     currPos = (currPos + 1) % 2;
+     flag = 0;
+   } else {
+     // Going the wrong way
+     flag = 1;
+   }
+
+ }
+ 
 
